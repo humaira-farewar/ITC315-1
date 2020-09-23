83a2663d390f566b1feede6ae24df797995f52f4
sediqa-s <49757897+sediqa-s@users.noreply.github.com>
2020-09-24 01:15:22 +0430

    Update java1.java

--- java1.java
+++ java1.java
@@ -1,4 +1,5 @@
 //this is a very simple java code that writes java for learning github
+// for extra exercise I am bringing changes to this file from the NewSeries branch and then I'll merge them 
 public class Exercise8 {
  
     public static void main(String[] args) {
